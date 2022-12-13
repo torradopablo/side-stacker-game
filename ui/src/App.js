3ce464e2fa9c3f171727c1bd33cf185b46a51654
torradopablo <torradopablo1@gmail.com>
2022-12-13 14:30:12 -0300

    Init project in develop

---
+++ ui/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="masthead" >
+      <img className="color-overlay"  src="background.jpg"></img>
+      
+    </div>
+  );
+}
+
+export default App;
