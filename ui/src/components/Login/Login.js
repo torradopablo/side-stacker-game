fcc988f891f53ee733e4f59c01dca91b68499cb0
pablo <pablotorrrado@torradodesarrollos.ar>
2022-12-29 20:41:47 -0300

    issue

--- ui/src/components/Login/Login.js
+++ ui/src/components/Login/Login.js
@@ -1,20 +1,36 @@
 import './login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Form } from 'react-bootstrap';
+import { Form , Button} from 'react-bootstrap';
 
 
 function LoginComponent() {
   return (
-    <div className='login-component-container' >
-        <Form className='rounded p-4 p-sm-3'>
-            <Form.Group 
-                className='mb-3'
-                controlId='formBasicEmail'
-            >test
-            </Form.Group>
-        </Form>
+    <div className='login-component-container'>
+      <div className='login-component-form' >
+          <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </div>
     </div>
+    
   );
 }
 
